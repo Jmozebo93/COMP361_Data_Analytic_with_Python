c93677803a08517f7509614c7144d27cbbff8468
Jordan Mozebo <73420348+Jmozebo93@users.noreply.github.com>
2023-09-12 11:31:52 -0400

    Function that slices a list that has been passed to it

--- my_slice.py
+++ my_slice.py
@@ -6,7 +6,7 @@ Created on Mon Sep 11 09:28:18 2023
 @author: jordanmozebo
 """
 
-def my_slice(lst, start, step, length):
+def my_slice(lst, start = 0, step = 2, length = None):
     if length is None:
         return lst[start::step]
     else:
